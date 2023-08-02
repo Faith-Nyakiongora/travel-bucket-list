a32d6fd3592ddde490cf480d8a60eb9ee710de38
Kibetian <126007714+Kibetian@users.noreply.github.com>
2023-08-02 21:58:21 +0300

    more details

--- src/Components/DestinationCard.js
+++ src/Components/DestinationCard.js
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 function DestinationCard({ place }) {
-  const { id, name, image_url, description, location } = place;
+  const { id, name, image_url, description, location, more } = place;
+
+  const [showDescription, setShowDescription] = useState(false);
+
+  const handleCardClick = () => {
+    setShowDescription(!showDescription);
+  };
+
   return (
-    <div key={id} className="card col-4">
+    <div key={id} className="card col-4" onClick={handleCardClick}>
       <img src={image_url} className="card-img-top" alt=".." />
       <div className="card-body">
         <h4 className="card-title">{name}</h4>
-        <p ClassName="card-text">{location}</p>
-        <p className="card-text">{description}</p>
+        <p className="card-text">{location}</p>
+        {showDescription && <p className="card-text">
+          {description}
+          {more}
+          </p>}
       </div>
       <div className="card-footer">
-        <button className="btn btn-sm btn-success">More details</button>
+        {/* <button className="btn btn-sm btn-success">More details</button> */}
       </div>
     </div>
   );
