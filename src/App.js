0fca9d7702160340b8fe03a6aefcf896d698c535
+ <+>
2023-08-03 23:44:12 +0300

    App.js route-faith

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import logo from './logo.svg';
 // import './App.css';
 import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DestinationList from "./Components/DestinationsList";
 import Navbar from "./Components/Navbar";
 
@@ -11,11 +12,18 @@ function App() {
     <div className="container">
       <Navbar />
       <h1>Destinations:</h1>
-
-      <DestinationList
-        destination={destination}
-        setDestination={setDestination}
-      />
+      <Routes>
+        <Route
+          path="/DestinationList"
+          element={
+            <DestinationList
+              destination={destination}
+              setDestination={setDestination}
+            />
+          }
+        />
+        {/* Add other routes if needed */}
+      </Routes>
     </div>
   );
 }
