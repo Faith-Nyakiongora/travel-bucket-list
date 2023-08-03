15746a5fad8250910418c01c7c1961b3eff4754d
+ <+>
2023-08-04 00:22:07 +0300

    json and App

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 // import logo from './logo.svg';
 // import './App.css';
+
 import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DestinationList from "./Components/DestinationsList";
+import Navbar from "./Components/Navbar";
 
 function App() {
   const [destination, setDestination] = useState([]);
 
   return (
     <div className="container">
+      <Navbar />
       <h1>Destinations:</h1>
-
-      <DestinationList
-        destination={destination}
-        setDestination={setDestination}
-      />
+      <Routes>
+        <Route
+          path="/DestinationList"
+          element={
+            <DestinationList
+              destination={destination}
+              setDestination={setDestination}
+            />
+          }
+        />
+        {/* Add other routes if needed */}
+      </Routes>
     </div>
   );
 }
