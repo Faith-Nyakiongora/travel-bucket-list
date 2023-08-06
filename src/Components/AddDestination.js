8ba6093bb7f1c0706a234b71cec46d2a58374be7
+ <+>
2023-08-06 03:58:37 +0300

    styling add destination

--- src/Components/AddDestination.js
+++ src/Components/AddDestination.js
@@ -1,4 +1,8 @@
 import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
 
 function AddDestination({ addDestination }) {
   const [name, setName] = useState("");
@@ -19,30 +23,67 @@ function AddDestination({ addDestination }) {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="Location"
-          value={location}
-          onChange={(e) => setLocation(e.target.value)}
-        />
-        <input
+    <Form onSubmit={handleSubmit}>
+      <Row className="mb-3">
+        <Form.Group as={Col} controlId="formGridEmail">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter Name"
+            onChange={(e) => setName(e.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridPassword">
+          <Form.Label>Location</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Location"
+            onChange={(e) => setLocation(e.target.value)}
+          />
+        </Form.Group>
+      </Row>
+
+      <Form.Group className="mb-3" controlId="formGridAddress1">
+        <Form.Label>Description</Form.Label>
+        <Form.Control
           type="text"
           placeholder="Description"
-          value={description}
           onChange={(e) => setDescription(e.target.value)}
         />
-        <button type="submit">Add Destination</button>
-      </form>
-    </div>
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Add Destination
+      </Button>
+    </Form>
   );
 }
 
 export default AddDestination;
+
+//     // <div>
+//     //   <form onSubmit={handleSubmit}>
+//     //     <input
+//     //       type="text"
+//     //       placeholder="Name"
+//     //       value={name}
+//     //       onChange={(e) => setName(e.target.value)}
+//     //     />
+//     //     <input
+//     //       type="text"
+//     //       placeholder="Location"
+//     //       value={location}
+//     //       onChange={(e) => setLocation(e.target.value)}
+//     //     />
+//     //     <input
+//     //       type="text"
+//     //       placeholder="Description"
+//     //       value={description}
+//     //       onChange={(e) => setDescription(e.target.value)}
+//     //     />
+//     //     <button type="submit">Add Destination</button>
+//     //   </form>
+//     // </div>
+//   );
+// }
